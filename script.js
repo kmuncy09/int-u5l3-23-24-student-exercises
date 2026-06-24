b0238b474521570b32bb2ab0151b4c6cc3a23b19
kmuncy09 <kendall.muncy09@gmail.com>
2026-06-24 13:27:18 -0500

    5.3

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let body = document.querySelector("body");
 
 // Array of image links
-let photos = ["https://preview.redd.it/if-the-show-is-successful-what-would-be-good-seasonal-pacing-v0-pwepl905kdlb1.jpg?width=640&crop=smart&auto=webp&s=1ccf2f03a4a9cee216bfdf27fdfac95d28392fd9",
-    "https://i1.wp.com/www.oxfordstudent.com/wp-content/uploads/2016/11/black-mirror-season-3-poster.png?fit=1080%2C1080&ssl=1",
-    "https://upload.wikimedia.org/wikipedia/commons/3/38/Stranger_Things_logo.png",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Grey%27s_Anatomy_Logo.svg/480px-Grey%27s_Anatomy_Logo.svg.png",
-    "https://assets.fxnetworks.com/cms-next/production/67a0aace-e484-4032-885b-c6f6a405c7eb.jpg"
+let photos = ["https://upload.wikimedia.org/wikipedia/en/5/5a/The_Big_Bang_Theory_Season_8.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/a/a0/Modern_Family_season_11_poster.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Superstore_%28Universal_Television_sitcom%29.svg/500px-Superstore_%28Universal_Television_sitcom%29.svg.png",
+    "https://www.publicbooks.org/wp-content/uploads/2020/04/MV5BMWMxYzBlNWMtYjRlNC00ZTY1LWE4ZTktMzMxYjlhODkwYWRhXkEyXkFqcGdeQXVyNjc5Mjg0NjU@._V1_-1000x600.jpg",
+    "https://static01.nyt.com/images/2020/11/24/arts/23gilmoregirls1/23gilmoregirls1-superJumbo.jpg?quality=75&auto=webp",
 ];
 
 
 // 1. Push an image link of one of your favorite shows to the photos array.
 //  - To find the link, right-click an image and choose 'Copy Image Address'
-
+photos.push ("https://upload.wikimedia.org/wikipedia/en/4/43/SwitchedatBirthCast.jpg");
 
 
 // HTML selectors
@@ -25,3 +25,8 @@ let show6 = document.querySelector(".show-6");
 
 show1.src = photos[0];
 // 2. Update the src for each show# variable - use the line above as a reference.
+show2.src= photos[1];
+show3.src= photos[2];
+show4.src= photos[3];
+show5.src= photos[4];
+show6.src= photos[5];
